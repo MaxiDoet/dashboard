c5f695fbd7d0bf05a70bca6bfbb20c92740da9c6
TheHeroCraft1579 <maximilian.doetsch@outlook.de>
2021-07-30 21:10:56 +0200

    Added spotify client

--- main.py
+++ main.py
@@ -7,12 +7,11 @@ import utils
 from time import sleep
 import json
 
-import nextion
+from nextion.display import NextionDisplay
 from data import hardware
 from data.weather import WeatherApiClient
 import system
 import utils
-import effects
 
 # If we're on windows platform we need to init colorama
 if system.is_win():
@@ -57,26 +56,72 @@ def detect_sensors():
 
 detect_sensors()
 
+info("Enabling features")
+for feature in config["features"]:
+    if not config["features"][feature]["enabled"]:
+        continue
+
+    if feature == "spotify":
+        info("Spotify Client (client_id=%s)" % config["features"]["spotify"]["client_id"])
+        import spotipy
+        import spotipy.util as util
+
+        client_id = config["features"]["spotify"]["client_id"]
+        client_secret = config["features"]["spotify"]["client_secret"]
+        scope = 'user-read-currently-playing user-read-playback-state'
+        token = util.prompt_for_user_token("maximilian.doetsch", scope, client_id=client_id, client_secret=client_secret, redirect_uri="http://localhost/callback")
+        sp = spotipy.Spotify(auth=token)
+
+        spotify_last_id = None
+
 #weather_client = WeatherApiClient(config["weatherApiKey"], config["weatherLocation"])
 
 # Init display
 info("Connecting to nextion display (%s)" % config["serialPort"])
 try:
-    display = nextion.NextionDisplay(config["serialPort"], 115200)
+    display = NextionDisplay(config["serialPort"], 115200, debug_enabled=False)
+    display_info = display.connect()
+
+    info("Display Info: Model: %s" % display_info["model"])
+
     info("Connected to nextion display")
 except:
-    error("Could not connect to display")
+    err("Could not connect to display")
     sys.exit()
 
 display.reset()
+display.write("dim=0")
 display.write("sleep=0")
+display.write("ussp=10")
+display.write("thup=0")
+
+for i in range(99):
+    sleep(0.005)
+    display.write("dim=%s" % i)
 
 while True:
     try:
         sleep(config["updateInterval"])
         temp = hardware.get_cpu_temperature()
-        info("CPU Temp: %s" % temp)
         display.write('t0.txt="%s"' % (str(temp)))
+        
+        # Spotify
+        spotify_current_playback = sp.current_playback()
+        spotify_current_id = spotify_current_playback["item"]["id"]
+        if spotify_current_id != spotify_last_id:
+            info("Spotify playing: Device: %s%s%s Title: %s%s%s Artist: %s%s%s" % (
+                colorama.Fore.GREEN, \
+                spotify_current_playback["device"]["name"], \
+                colorama.Fore.RESET, \
+                colorama.Fore.GREEN, \
+                spotify_current_playback["item"]["name"], \
+                colorama.Fore.RESET, \
+                colorama.Fore.GREEN, \
+                spotify_current_playback["item"]["artists"][0]["name"], \
+                colorama.Fore.RESET))
+
+        spotify_last_id = spotify_current_id
+
         display.handle_events()
     except KeyboardInterrupt:
         if watchdog:
--- nextion/display.py
+++ nextion/display.py
@@ -1,4 +1,6 @@
 import serial
+from time import sleep
+from log import *
 
 nextion_cmd_end = bytearray([0xff, 0xff, 0xff])
 
@@ -8,12 +10,37 @@ class NextionDisplay():
         self.baud_rate = baud_rate
         self.debug_enabled = debug_enabled
         self.event_handlers = []
+        
+        self.connected = False
 
+    def connect(self):
         # Init serial dev
         self.serial_dev = serial.Serial(self.serial_port, self.baud_rate, timeout=2)
 
+        # Get info
+        self.write("connect")
+        sleep(2)
+        result = self.read()
+
+        info_object = {'touch': 0, 'reserved': 0, 'model': "", 'firmware_version': 0, 'mcu_code': 0, 'serial_number': 0, 'flash_size': 0}
+
+        if result[:5] == b'comok':
+            result = result[6:].decode().split(",")
+            info_object["touch"] = result[0]
+            info_object["reserved"] = result[1]
+            info_object["model"] = result[2]
+            info_object["firmware_version"] = result[3]
+            info_object["mcu_code"] = result[4]
+            info_object["serial_number"] = result[5]
+            info_object["flash_size"] = result[6]
+
+        self.connected = True
+        return info_object
+
     def read(self):
         data = self.serial_dev.read_all()
+        # Remove eof
+        data = data[:-3]
 
         if self.debug_enabled:
             print("RX <-- NEXTION: %s" % data)
